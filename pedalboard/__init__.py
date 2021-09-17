332335e8b4ff07bb9ef2345f3550110fc5208c47
Peter Sobot <psobot@spotify.com>
2021-09-17 00:44:05 -0400

    Update __init__.py

--- pedalboard/__init__.py
+++ pedalboard/__init__.py
@@ -17,7 +17,7 @@
 
 from pedalboard_native import *  # noqa: F403, F401
 from .pedalboard import Pedalboard, AVAILABLE_PLUGIN_CLASSES, load_plugin  # noqa: F401
-from .version import __version__ # noqa: F401
+from .version import __version__  # noqa: F401
 
 for klass in AVAILABLE_PLUGIN_CLASSES:
     vars()[klass.__name__] = klass
