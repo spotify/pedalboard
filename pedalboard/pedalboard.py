5a379bc0647186c43004e1c456868d9ad247c36f
Peter Sobot <psobot@spotify.com>
2021-12-19 10:58:10 -0500

    Fix Black code formatting on Python 3.10.

--- pedalboard/pedalboard.py
+++ pedalboard/pedalboard.py
@@ -638,7 +638,6 @@ try:
             super().__init__(path_to_plugin_file)
             self.__set_initial_parameter_values__(parameter_values)
 
-
 except ImportError:
     # We may be on a system that doesn't have native AudioUnitPlugin support.
     # (i.e.: any platform that's not macOS.)
