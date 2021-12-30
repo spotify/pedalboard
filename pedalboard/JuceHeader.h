eff8dab35ee9000e0795e6d724e9820db1622605
Hussain Abdi <hussainabdi12@gmail.com>
2021-12-30 17:42:13 -0500

    Updated JUCE to version 6.1.4 (#55)
    * Updated JUCE to version 6.1.4
    
    * Manually Update JUCE_PROJUCER_VERSION
    
    * Removed JUCE_PROJUCER_VERSION

--- pedalboard/JuceHeader.h
+++ pedalboard/JuceHeader.h
@@ -31,16 +31,3 @@
 #include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
 #include <juce_gui_basics/juce_gui_basics.h>
-
-#define JUCE_PROJUCER_VERSION 0x60008
-
-#if JUCE_PROJUCER_VERSION < JUCE_VERSION
-/** If you've hit this error then the version of the Projucer that was used to
-   generate this project is older than the version of the JUCE modules being
-   included. To fix this error, re-save your project using the latest version of
-   the Projucer or, if you aren't using the Projucer to manage your project,
-    remove the JUCE_PROJUCER_VERSION define from the AppConfig.h file.
-*/
-#error                                                                         \
-    "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
-#endif
