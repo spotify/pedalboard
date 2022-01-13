fcfc3c5abc85abcdf61a6b0f102a4cf28f82e180
John Lingi <jlingi@spotify.com>
2022-01-13 19:27:31 +0000

    Wrote process function for rubberband

--- pedalboard/RubberbandPlugin.h
+++ pedalboard/RubberbandPlugin.h
@@ -16,7 +16,7 @@ namespace Pedalboard
   public:
     virtual ~RubberbandPlugin(){};
 
-    virtual void prepare(const juce::dsp::ProcessSpec &spec) = 0;
+    // virtual void prepare(const juce::dsp::ProcessSpec &spec) = 0;
 
     void process(
         const juce::dsp::ProcessContextReplacing<float> &context) override final
@@ -26,19 +26,50 @@ namespace Pedalboard
       {
         // Is this the right way to get the next input block?
         // Do I have to shift something?
-        size_t sampleFrames = 0; // How do I compute this? Is there a value somewhere
-        auto inputBlock = context.getInputBlock();
-        // rbPtr->process(, sampleFrames, false); // Impact of not setting final to true?
-        // size_t samplesRetrieved = 0;
-        // size_t samplesAvailable = 0;
-        // while (samplesRetrieved < inputBlock.getNumSamples()) // is this correct to hang here for each block?
-        // {
-        //   samplesAvailable = rbPtr->available();
-        //   if (samplesAvailable)
-        //   {
-        //     samplesRetrieved += rbPtr->retrieve(context.getOutputBlock(), samplesAvailable);
-        //   }
-        // }
+        // size_t sampleFrames = 0; // How do I compute this? Is there a value somewhere
+
+        auto inBlock = context.getInputBlock();
+        auto outBlock = context.getOutputBlock();
+
+        // TODO: Is num samples the total number of frames or n_samples_per_channel * n_channels
+        auto len = inBlock.getNumSamples();
+        auto numChans = inBlock.getNumChannels();
+
+        jassert(len == outBlock.getNumSamples());
+        jassert(numChans == outBlock.getNumChannels());
+
+        // Have to find way to get all channel data?
+        const float *inChannels[numChans];
+        float *outChannels[numChans];
+        for (size_t i = 0; i < numChans; i++)
+        {
+          inChannels[i] = inBlock.getChannelPointer(i);
+          outChannels[i] = outBlock.getChannelPointer(i);
+          std::cout << "input channel " << i << " = " << inChannels[i] << "\n";
+          std::cout << "output channel " << i << " = " << outChannels[i] << "\n";
+        }
+
+        // Rubberband expects all channel data with one float array per channel
+        processSamples(inChannels, outChannels, len, false);
+      }
+    }
+
+    void processSamples(const float *const *inBlock, float *const *outBlock, size_t samples, bool final)
+    {
+      rbPtr->process(inBlock, samples, false); // Impact of not setting final to true?
+      std::cout << rbPtr->getSamplesRequired() << "\n";
+      size_t samplesRetrieved = 0;
+      size_t samplesAvailable = 0;
+      int count = 0;
+      while (count < 10) // is this correct to hang here for each block?
+      {
+        samplesAvailable = rbPtr->available();
+        std::cout << samplesAvailable << "\n";
+        if (samplesAvailable)
+        {
+          samplesRetrieved += rbPtr->retrieve(outBlock, samplesAvailable);
+        }
+        count++;
       }
     }
 
@@ -54,7 +85,7 @@ namespace Pedalboard
     // RubberBandStretcher *getRb() { return rbPtr; }
 
   protected:
-    // TODO: Do I need the DSP block? or are the buffers handled by RB?
+    // TODO: Do I need the DSP block?
     // DSPType dspBlock;
     RubberBandStretcher *rbPtr = nullptr;
   };
