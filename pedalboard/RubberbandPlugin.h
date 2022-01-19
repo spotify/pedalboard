4f593467b136eb5ec3e73c899e0644408fa51680
John Lingi <jlingi@spotify.com>
2022-01-19 18:57:32 +0000

    Format rubberband plugin code

--- pedalboard/RubberbandPlugin.h
+++ pedalboard/RubberbandPlugin.h
@@ -3,93 +3,83 @@
 
 using namespace RubberBand;
 
-namespace Pedalboard
-{
-  class RubberbandPlugin : public Plugin
-  /*
+namespace Pedalboard {
+class RubberbandPlugin : public Plugin
+/*
 Base class for rubberband plugins.
 */
-  {
-  public:
-    virtual ~RubberbandPlugin(){};
+{
+public:
+  virtual ~RubberbandPlugin(){};
 
-    void process(
-        const juce::dsp::ProcessContextReplacing<float> &context) override final
-    {
-      if (rbPtr)
-      {
-        auto inBlock = context.getInputBlock();
-        auto outBlock = context.getOutputBlock();
+  void process(
+      const juce::dsp::ProcessContextReplacing<float> &context) override final {
+    if (rbPtr) {
+      auto inBlock = context.getInputBlock();
+      auto outBlock = context.getOutputBlock();
 
-        auto len = inBlock.getNumSamples();
-        auto numChannels = inBlock.getNumChannels();
+      auto len = inBlock.getNumSamples();
+      auto numChannels = inBlock.getNumChannels();
 
-        jassert(len == outBlock.getNumSamples());
-        jassert(numChannels == outBlock.getNumChannels());
+      jassert(len == outBlock.getNumSamples());
+      jassert(numChannels == outBlock.getNumChannels());
 
-        const float *inChannels[numChannels];
-        float *outChannels[numChannels];
-        for (size_t i = 0; i < numChannels; i++)
-        {
-          inChannels[i] = inBlock.getChannelPointer(i);
-          outChannels[i] = outBlock.getChannelPointer(i);
+      const float *inChannels[numChannels];
+      float *outChannels[numChannels];
+      for (size_t i = 0; i < numChannels; i++) {
+        inChannels[i] = inBlock.getChannelPointer(i);
+        outChannels[i] = outBlock.getChannelPointer(i);
+      }
+
+      // Rubberband expects all channel data with one float array per channel
+      processSamples(inChannels, outChannels, len, numChannels);
+    }
+  }
+
+  void reset() override final {
+    if (rbPtr) {
+      rbPtr->reset();
+    }
+  }
+
+private:
+  void processSamples(const float *const *inBlock, float **outBlock,
+                      size_t samples, size_t numChannels) {
+    // Push all of the input samples into RubberBand:
+    rbPtr->process(inBlock, samples, false);
+
+    // Figure out how many samples RubberBand is ready to give to us:
+    int availableSamples = rbPtr->available();
+
+    // ...but only actually ask Rubberband for at most the number of samples we
+    // can handle:
+    int samplesToPull = samples;
+    if (samplesToPull > availableSamples)
+      samplesToPull = availableSamples;
+
+    // If we don't have enough samples to fill a full buffer,
+    // right-align the samples that we do have (i..e: start with silence).
+    int missingSamples = samples - availableSamples;
+    if (missingSamples > 0) {
+      for (size_t c = 0; c < numChannels; c++) {
+        // Clear the start of the buffer so that we start
+        // the buffer with silence:
+        for (size_t i = 0; i < missingSamples; i++) {
+          outBlock[c][i] = 0.0;
         }
 
-        // Rubberband expects all channel data with one float array per channel
-        processSamples(inChannels, outChannels, len, numChannels);
+        // Move the output buffer pointer forward so that
+        // RubberBandStretcher::retrieve(...) places its
+        // output at the end of the buffer:
+        outBlock[c] += missingSamples;
       }
     }
 
-    void reset() override final
-    {
-      if (rbPtr)
-      {
-        rbPtr->reset();
-      }
-    }
+    // Pull the next audio data out of Rubberband:
+    rbPtr->retrieve(outBlock, samplesToPull);
+  }
 
-  private:
-    void processSamples(const float *const *inBlock, float **outBlock,
-                        size_t samples, size_t numChannels)
-    {
-      // Push all of the input samples into RubberBand:
-      rbPtr->process(inBlock, samples, false);
-
-      // Figure out how many samples RubberBand is ready to give to us:
-      int availableSamples = rbPtr->available();
-
-      // ...but only actually ask Rubberband for at most the number of samples we
-      // can handle:
-      int samplesToPull = samples;
-      if (samplesToPull > availableSamples)
-        samplesToPull = availableSamples;
-
-      // If we don't have enough samples to fill a full buffer,
-      // right-align the samples that we do have (i..e: start with silence).
-      int missingSamples = samples - availableSamples;
-      if (missingSamples > 0)
-      {
-        for (size_t c = 0; c < numChannels; c++)
-        {
-          // Clear the start of the buffer so that we start
-          // the buffer with silence:
-          for (size_t i = 0; i < missingSamples; i++)
-          {
-            outBlock[c][i] = 0.0;
-          }
-
-          // Move the output buffer pointer forward so that
-          // RubberBandStretcher::retrieve(...) places its
-          // output at the end of the buffer:
-          outBlock[c] += missingSamples;
-        }
-      }
-
-      // Pull the next audio data out of Rubberband:
-      rbPtr->retrieve(outBlock, samplesToPull);
-    }
-
-  protected:
-    std::unique_ptr<RubberBandStretcher> rbPtr;
-  };
+protected:
+  std::unique_ptr<RubberBandStretcher> rbPtr;
+};
 }; // namespace Pedalboard
--- pedalboard/plugins/PitchShift.h
+++ pedalboard/plugins/PitchShift.h
@@ -22,65 +22,59 @@ namespace py = pybind11;
 
 #include "../RubberbandPlugin.h"
 
-namespace Pedalboard
-{
-    class PitchShift : public RubberbandPlugin
-    /*
+namespace Pedalboard {
+class PitchShift : public RubberbandPlugin
+/*
 Modifies pitch of an audio without affecting duration
 */
-    {
-    private:
-        double _scaleFactor = 1.0;
+{
+private:
+  double _scaleFactor = 1.0;
 
-    public:
-        void setScaleFactor(double scale)
-        {
-            if (scale <= 0)
-            {
-                throw std::range_error("Pitch scale must be a value greater than 0.0.");
-            }
-            _scaleFactor = scale;
-            if (rbPtr)
-                rbPtr->setPitchScale(_scaleFactor);
-        }
-
-        double getScaleFactor() { return _scaleFactor; }
-
-        void prepare(const juce::dsp::ProcessSpec &spec) override final
-        {
-            bool specChanged = lastSpec.sampleRate != spec.sampleRate ||
-                               lastSpec.maximumBlockSize < spec.maximumBlockSize ||
-                               spec.numChannels != lastSpec.numChannels;
-
-            if (!rbPtr || specChanged)
-            {
-                auto stretcherOptions = RubberBandStretcher::OptionProcessRealTime |
-                                        RubberBandStretcher::OptionThreadingNever;
-                rbPtr = std::make_unique<RubberBandStretcher>(spec.sampleRate, spec.numChannels,
-                                                              stretcherOptions);
-                rbPtr->setMaxProcessSize(spec.maximumBlockSize);
-                rbPtr->setPitchScale(_scaleFactor);
-                rbPtr->reset();
-                lastSpec = spec;
-            }
-        }
-    };
-
-    inline void init_pitch_shift(py::module &m)
-    {
-        py::class_<PitchShift, Plugin>(
-            m, "PitchShift",
-            "Shift pitch without affecting audio duration. Shifted audio may start "
-            "with a short duration of silence, depending on the scale factor used. "
-            "The rate of the audio will be multiplied by scale_factor (i.e.: 2x "
-            "means one octave up, 0.5x means one octave down).")
-            .def(py::init([](double scale)
-                          {
-                              auto plugin = new PitchShift();
-                              plugin->setScaleFactor(scale);
-                              return plugin;
-                          }),
-                 py::arg("scale_factor") = 1.0)
-            .def_property("scale_factor", &PitchShift::getScaleFactor, &PitchShift::setScaleFactor);
+public:
+  void setScaleFactor(double scale) {
+    if (scale <= 0) {
+      throw std::range_error("Pitch scale must be a value greater than 0.0.");
     }
+    _scaleFactor = scale;
+    if (rbPtr)
+      rbPtr->setPitchScale(_scaleFactor);
+  }
+
+  double getScaleFactor() { return _scaleFactor; }
+
+  void prepare(const juce::dsp::ProcessSpec &spec) override final {
+    bool specChanged = lastSpec.sampleRate != spec.sampleRate ||
+                       lastSpec.maximumBlockSize < spec.maximumBlockSize ||
+                       spec.numChannels != lastSpec.numChannels;
+
+    if (!rbPtr || specChanged) {
+      auto stretcherOptions = RubberBandStretcher::OptionProcessRealTime |
+                              RubberBandStretcher::OptionThreadingNever;
+      rbPtr = std::make_unique<RubberBandStretcher>(
+          spec.sampleRate, spec.numChannels, stretcherOptions);
+      rbPtr->setMaxProcessSize(spec.maximumBlockSize);
+      rbPtr->setPitchScale(_scaleFactor);
+      rbPtr->reset();
+      lastSpec = spec;
+    }
+  }
+};
+
+inline void init_pitch_shift(py::module &m) {
+  py::class_<PitchShift, Plugin>(
+      m, "PitchShift",
+      "Shift pitch without affecting audio duration. Shifted audio may start "
+      "with a short duration of silence, depending on the scale factor used. "
+      "The rate of the audio will be multiplied by scale_factor (i.e.: 2x "
+      "means one octave up, 0.5x means one octave down).")
+      .def(py::init([](double scale) {
+             auto plugin = new PitchShift();
+             plugin->setScaleFactor(scale);
+             return plugin;
+           }),
+           py::arg("scale_factor") = 1.0)
+      .def_property("scale_factor", &PitchShift::getScaleFactor,
+                    &PitchShift::setScaleFactor);
+}
 }; // namespace Pedalboard
