e7a7569e7b251b0ef815f503d46597a5d322fb47
Peter Sobot <psobot@spotify.com>
2022-02-06 19:10:15 -0500

    Reformatting.

--- pedalboard/plugin_templates/ForceMono.h
+++ pedalboard/plugin_templates/ForceMono.h
@@ -107,11 +107,8 @@ public:
 using ForceMonoTestPlugin = ForceMono<ExpectsMono>;
 
 inline void init_force_mono_test_plugin(py::module &m) {
-  py::class_<ForceMonoTestPlugin, Plugin>(m,
-                                                 "ForceMonoTestPlugin")
-      .def(py::init([]() {
-             return std::make_unique<ForceMonoTestPlugin>();
-           }))
+  py::class_<ForceMonoTestPlugin, Plugin>(m, "ForceMonoTestPlugin")
+      .def(py::init([]() { return std::make_unique<ForceMonoTestPlugin>(); }))
       .def("__repr__", [](const ForceMonoTestPlugin &plugin) {
         std::ostringstream ss;
         ss << "<pedalboard.ForceMonoTestPlugin";
