d4424275c9666c3df007704f3d49e2c8ed92e694
Peter Sobot <psobot@spotify.com>
2022-07-11 15:13:10 +0100

    Remove redeclaration of uint/int_ types.

--- vendors/lame_config.h
+++ vendors/lame_config.h
@@ -71,18 +71,6 @@ typedef float float32_t;
 typedef double float64_t;
 #elif defined(__GNUC__)
 #define __int8_t_defined
-
-#ifndef __DEFINED___uint32_t
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-#endif
-
 #endif
 
 typedef long double ieee854_float80_t;
