8c58f41fd2962d2156341226b8ee298f6ceacfad
Peter Sobot <psobot@spotify.com>
2022-08-15 15:51:40 -0400

    Try factoring out test code to avoid GC issues.

--- tests/test_memory_leaks.py
+++ tests/test_memory_leaks.py
@@ -22,17 +22,22 @@ import pytest
 from .test_external_plugins import load_test_plugin, AVAILABLE_PLUGINS_IN_TEST_ENVIRONMENT
 
 
+def get_first_parameter_from(plugin):
+    param_name, param = next(
+        iter([(name, param) for name, param in plugin.parameters.items() if param.type is float])
+    )
+    value = getattr(plugin, param_name)
+    return param_name, param, value
+
+
 @pytest.mark.parametrize("plugin_path", AVAILABLE_PLUGINS_IN_TEST_ENVIRONMENT)
 def test_plugin_can_be_garbage_collected(plugin_path: str):
     # Load a VST3 or Audio Unit plugin from disk:
     plugin = load_test_plugin(plugin_path, disable_caching=True)
 
     _plugin_ref = weakref.ref(plugin)
-    param_name, param = next(
-        iter([(name, param) for name, param in plugin.parameters.items() if param.type is float])
-    )
-    value = getattr(plugin, param_name)
 
+    param_name, param, value = get_first_parameter_from(plugin)
     initial_value = float(value)
 
     # After the plugin is no longer in scope, it should be garbage collectable
