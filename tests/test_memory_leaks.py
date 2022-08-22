8c0ad9677f64c8511c27298a078cca31821e1e83
Peter Sobot <psobot@spotify.com>
2022-08-22 11:57:31 -0400

    Add Python 3.11.0-rc1 support. (#139)
    * Try adding Python 3.11.0-rc1 support.
    
    * Update all.yml
    
    * Remove win_arm64 builds.
    
    * Add debugging info to weakref test.
    
    * Try factoring out test code to avoid GC issues.
    
    * Avoid using py::dynamic_attr to avoid Py3.11 incompatibility.
    
    Should be fixed in PyBind11: https://github.com/pybind/pybind11/commit/6abb7de6cd12d7a3b6d3afbdf98f3795639466a3.
    
    * Re-add upload path to upload action.
    
    * Ignore CSS when comparing docs to expected values.
    
    * Don't generate type stubs for documentation on Py3.11.
    
    * Remove ubuntu-18.04.
    
    * Bump to v0.5.10.
    
    * Bump documentation version.

--- tests/test_memory_leaks.py
+++ tests/test_memory_leaks.py
@@ -22,17 +22,22 @@ import pytest
 from .test_external_plugins import load_test_plugin, AVAILABLE_PLUGINS_IN_TEST_ENVIRONMENT
 
 
+def get_first_parameter_from(plugin):
+    param_name, param = next(
+        iter([(name, param) for name, param in plugin.parameters.items() if param.type is float])
+    )
+    value = getattr(plugin, param_name)
+    return param_name, param, value
+
+
 @pytest.mark.parametrize("plugin_path", AVAILABLE_PLUGINS_IN_TEST_ENVIRONMENT)
 def test_plugin_can_be_garbage_collected(plugin_path: str):
     # Load a VST3 or Audio Unit plugin from disk:
     plugin = load_test_plugin(plugin_path, disable_caching=True)
 
     _plugin_ref = weakref.ref(plugin)
-    param_name, param = next(
-        iter([(name, param) for name, param in plugin.parameters.items() if param.type is float])
-    )
-    value = getattr(plugin, param_name)
 
+    param_name, param, value = get_first_parameter_from(plugin)
     initial_value = float(value)
 
     # After the plugin is no longer in scope, it should be garbage collectable
@@ -49,6 +54,13 @@ def test_plugin_can_be_garbage_collected(plugin_path: str):
     # Once we delete that parameter, the plugin should be releasable
     del param
     gc.collect()
-    assert not _plugin_ref()
+
+    # Do this dance because calling gc.get_referrers with None would return a huge amount:
+    referrers = None
+    if _plugin_ref() is not None:
+        referrers = gc.get_referrers(_plugin_ref())
+
+    assert _plugin_ref() is None, f"Expected plugin to have no referrers, but found: {referrers}"
+
     assert set(dir(initial_value)).issubset(set(dir(value)))
     assert value == initial_value  # value should still not change
