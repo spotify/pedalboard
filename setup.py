395490210a7882c67249906957256f3552f51d34
Peter Sobot <psobot@spotify.com>
2022-08-22 13:04:18 -0400

    Add Python 3.11 to PyPI classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -325,6 +325,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     ext_modules=[pedalboard_cpp],
     install_requires=['numpy'],
