6c7f7afbdfdbc5425444306486a8e046a5748903
Peter Sobot <psobot@spotify.com>
2022-11-18 15:30:40 -0500

    Patch MP3 parser to ignore files that contain Lyrics3 data. (#164)
    * Patch MP3 parser to ignore files that contain Lyrics3 data.
    
    * More tests.
    
    * Bump clang-format version.
    
    * Bump version.

--- pedalboard/io/AudioFile.h
+++ pedalboard/io/AudioFile.h
@@ -17,9 +17,58 @@
 
 #pragma once
 
+#include "../juce_overrides/juce_PatchedMP3AudioFormat.h"
+#include "AudioFile.h"
+#include "LameMP3AudioFormat.h"
+
 namespace Pedalboard {
 
 static constexpr const unsigned int DEFAULT_AUDIO_BUFFER_SIZE_FRAMES = 8192;
+
+/**
+ * Registers audio formats for reading and writing in a deterministic (but
+ * configurable) order. On different platforms, different formats are handled by
+ * different backends (i.e.: CoreAudioFormat handles MP3 on macOS, but only for
+ * reading) so this method allows us to ensure reproducibility in tests.
+ */
+void registerPedalboardAudioFormats(juce::AudioFormatManager &manager,
+                                    bool forWriting, bool crossPlatformOnly) {
+  manager.registerFormat(new juce::WavAudioFormat(), true);
+  manager.registerFormat(new juce::AiffAudioFormat(), false);
+
+#if JUCE_USE_FLAC
+  manager.registerFormat(new juce::FlacAudioFormat(), false);
+#endif
+
+#if JUCE_USE_OGGVORBIS
+  manager.registerFormat(new juce::OggVorbisAudioFormat(), false);
+#endif
+
+  if (forWriting) {
+    // Prefer our own custom MP3 format (which only writes, doesn't read) over
+    // MP3AudioFormat (which only reads, doesn't write)
+    manager.registerFormat(new LameMP3AudioFormat(), false);
+  } else {
+    // On macOS, CoreAudio can read MP3s better (more fault-tolerantly) than
+    // MP3AudioFormat can. But sometimes, we still want to use the built-in MP3
+    // reader so that we can get identical parsing behaviour on both macOS and
+    // Linux. To do so, we use this flag:
+
+    if (crossPlatformOnly) {
+      manager.registerFormat(new juce::MP3AudioFormat(), false);
+    } else {
+#if JUCE_MAC || JUCE_IOS
+      manager.registerFormat(new juce::CoreAudioFormat(), false);
+#else
+      manager.registerFormat(new juce::MP3AudioFormat(), false);
+#endif
+    }
+  }
+
+#if JUCE_USE_WINDOWS_MEDIA_FORMAT
+  manager.registerFormat(new juce::WindowsMediaAudioFormat(), false);
+#endif
+}
 
 class AudioFile {};
 
---
+++ pedalboard/juce_overrides/juce_PatchedMP3AudioFormat.h
@@ -0,0 +1,72 @@
+#include "../JuceHeader.h"
+/*
+  ==============================================================================
+
+   This file is part of the JUCE library.
+   Copyright (c) 2020 - Raw Material Software Limited
+
+   JUCE is an open source library subject to commercial or open-source
+   licensing.
+
+   By using JUCE, you agree to the terms of both the JUCE 6 End-User License
+   Agreement and JUCE Privacy Policy (both effective as of the 16th June 2020).
+
+   End User License Agreement: www.juce.com/juce-6-licence
+   Privacy Policy: www.juce.com/juce-privacy-policy
+
+   Or: You may also use this code under the terms of the GPL v3 (see
+   www.gnu.org/licenses).
+
+   JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+
+  ==============================================================================
+*/
+
+namespace juce {
+
+//==============================================================================
+/**
+    Software-based MP3 decoding format (doesn't currently provide an encoder).
+
+    IMPORTANT DISCLAIMER: By choosing to  compile the MP3 code into your
+   software, you do so AT YOUR OWN RISK! By doing so, you are agreeing that Raw
+   Material Software Limited is in no way responsible for any patent, copyright,
+   or other legal issues that you may suffer as a result.
+
+    The code in juce_PatchedMP3AudioFormat.cpp is NOT guaranteed to be free from
+   infringements of 3rd-party intellectual property. If you wish to use it,
+   please seek your own independent advice about the legality of doing so. If
+   you are not willing to accept full responsibility for the consequences of
+   using this code, then do not use this.
+
+    @tags{Audio}
+*/
+class PatchedMP3AudioFormat : public AudioFormat {
+public:
+  //==============================================================================
+  PatchedMP3AudioFormat();
+  ~PatchedMP3AudioFormat() override;
+
+  //==============================================================================
+  Array<int> getPossibleSampleRates() override;
+  Array<int> getPossibleBitDepths() override;
+  bool canDoStereo() override;
+  bool canDoMono() override;
+  bool isCompressed() override;
+  StringArray getQualityOptions() override;
+
+  //==============================================================================
+  AudioFormatReader *createReaderFor(InputStream *,
+                                     bool deleteStreamIfOpeningFails) override;
+
+  AudioFormatWriter *createWriterFor(OutputStream *, double sampleRateToUse,
+                                     unsigned int numberOfChannels,
+                                     int bitsPerSample,
+                                     const StringPairArray &metadataValues,
+                                     int qualityOptionIndex) override;
+  using AudioFormat::createWriterFor;
+};
+
+} // namespace juce
