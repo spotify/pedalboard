a7f7e977747ef2d28ba7bac39b47c0663ef980cc
Peter Sobot <psobot@spotify.com>
2023-02-04 16:18:32 -0500

    Again.

--- tests/mypy_fixtures/readme_quickstart.py
+++ tests/mypy_fixtures/readme_quickstart.py
@@ -8,10 +8,8 @@ board = Pedalboard([Chorus(), Reverb(room_size=0.25)])
 
 # Open an audio file for reading, just like a regular file:
 with AudioFile("some-file.wav") as f:
-
     # Open an audio file to write to:
     with AudioFile("output.wav", "w", f.samplerate, f.num_channels) as o:
-
         # Read one second of audio at a time, until the file is empty:
         while f.tell() < f.frames:
             chunk = f.read(int(f.samplerate))
