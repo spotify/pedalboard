adf76670ea6c974e482b3de56f8efefaab86a3f0
Peter Sobot <psobot@spotify.com>
2024-03-07 13:01:34 -0500

    Bump to v0.9.2.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.9.1',
+    VERSION: 'v0.9.2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
