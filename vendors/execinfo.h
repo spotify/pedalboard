b31f8e93d926405325fa95debb7441136a8a3d6f
Peter Sobot <psobot@spotify.com>
2024-05-18 14:14:15 -0400

    Add empty function bodies for execinfo.

--- vendors/execinfo.h
+++ vendors/execinfo.h
@@ -20,22 +20,20 @@
 
 #include <features.h>
 
-__BEGIN_DECLS
-
 /* Store up to SIZE return address of the current program state in
    ARRAY and return the exact number of values stored.  */
-extern int backtrace(void **__array, int __size) __nonnull((1));
+int backtrace(void **__array, int __size) __nonnull((1)) { return 0; }
 
 /* Return names of functions from the backtrace list in ARRAY in a newly
    malloc()ed memory block.  */
-extern char **backtrace_symbols(void *const *__array, int __size) __THROW
-    __nonnull((1));
+char **backtrace_symbols(void *const *__array, int __size) __THROW
+    __nonnull((1)) {
+  return NULL;
+}
 
 /* This function is similar to backtrace_symbols() but it writes the result
    immediately to a file.  */
-extern void backtrace_symbols_fd(void *const *__array, int __size,
-                                 int __fd) __THROW __nonnull((1));
-
-__END_DECLS
+void backtrace_symbols_fd(void *const *__array, int __size, int __fd) __THROW
+    __nonnull((1)) {}
 
 #endif /* execinfo.h  */
