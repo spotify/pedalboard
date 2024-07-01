b6bd4e37a7368d99c1ec170c12c78ecb1f3d70e2
Peter Sobot <psobot@spotify.com>
2024-07-01 15:32:01 -0400

    Enable optional AVX512 instructions.

--- setup.py
+++ setup.py
@@ -145,13 +145,26 @@ if platform.system() != "Darwin":
     # On ARM, ignore the X86-specific SIMD code:
     if "arm" in platform.processor() or "aarch64" in platform.processor():
         fftw_paths = ignore_files_matching(fftw_paths, "avx", "sse", "kcvi")
+        ALL_CFLAGS.append("-DHAVE_NEON=1")
     else:
         # And on x86, ignore the ARM-specific SIMD code:
         fftw_paths = ignore_files_matching(fftw_paths, "neon")
         # Support for FMA4 instructions:
         ALL_CFLAGS.append("-mfma4")
-        # Target Broadwell for the best SIMD support:
-        ALL_CFLAGS.append("-march=broadwell")
+        # Target Cannon Lake for reasonably modern SIMD support:
+        ALL_CFLAGS.append("-march=cannonlake")
+        # Enable SIMD instructions:
+        ALL_CFLAGS.extend(
+            [
+                "-DHAVE_SSE2",
+                "-DHAVE_AVX",
+                "-DHAVE_AVX_128_FMA",
+                "-DHAVE_AVX2",
+                "-DHAVE_AVX512",
+                "-DHAVE_GENERIC_SIMD128",
+                "-DHAVE_GENERIC_SIMD256",
+            ]
+        )
 
     fftw_paths.append(Path("vendors/fftw3/kernel/assert.c"))
     ALL_SOURCE_PATHS += fftw_paths
