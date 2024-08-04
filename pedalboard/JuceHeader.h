b0d4e9a10e2072106eecab767a5117451653cbef
Pedro <pedrolins1309@gmail.com>
2024-08-04 13:18:45 -0300

    Fix formatting

--- pedalboard/JuceHeader.h
+++ pedalboard/JuceHeader.h
@@ -23,6 +23,7 @@
 #pragma once
 
 #include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_core/juce_core.h>
@@ -31,4 +32,3 @@
 #include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
 #include <juce_gui_basics/juce_gui_basics.h>
-#include <juce_audio_devices/juce_audio_devices.h>
