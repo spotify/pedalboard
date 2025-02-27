b70b517e9d768707353ba5b598e741a8a7da3e4b
LorenzoMonti <lorenzomonti42@gmail.com>
2025-02-27 12:19:40 +0100

    The script test_fuzz.py modified. test_fuzz_process_silence() and test_fuzz_process_signal() methods added.

--- tests/test_fuzz.py
+++ tests/test_fuzz.py
@@ -1,5 +1,11 @@
 import pytest
+import numpy as np
 from pedalboard import Fuzz, Pedalboard
+
+NUM_SECONDS = 1
+MAX_SAMPLE_RATE = 96000
+NOISE = np.random.rand(int(NUM_SECONDS * MAX_SAMPLE_RATE)).astype(np.float32)
+
 
 def test_default_values():
     # Creates the Fuzz instance with default values ​​(drive_db=25, tone_hz=800)
@@ -33,6 +39,31 @@ def test_repr():
     assert "drive_db=35" in rep, "__repr__ must contains drive_db value"
     assert "tone_hz=950" in rep, "__repr__ must contains tone_hz value"
     assert "pedalboard.Fuzz" in rep, "__repr__ must indicate plugin type"
+
+
+def test_fuzz_process_silence():
+    fuzz = Fuzz()
+    sample_rate = 44100
+    num_samples = sample_rate  # mono audio - 1 sec
+    audio_in = np.zeros((num_samples, 1), dtype=np.float32)
+
+    audio_out = fuzz.process(audio_in, sample_rate)
+    np.testing.assert_array_almost_equal(audio_out, audio_in, decimal=5)
+
+
+def test_fuzz_process_signal():
+    drive_db = 30
+    tone_hz = 800
+    sample_rate = 44100
+    _input = NOISE[: int(NUM_SECONDS * sample_rate)]
+    fuzz = Fuzz(drive_db, tone_hz)
+    audio_in = np.column_stack((_input, _input))
+
+    audio_out = fuzz.process(audio_in, sample_rate)
+    assert audio_out.shape == audio_in.shape, "The form of the output must be the same as the input"
+    with pytest.raises(AssertionError):
+        np.testing.assert_array_almost_equal(audio_out, audio_in, decimal=5)
+
 
 @pytest.mark.parametrize("drive_db", [25., 35., 45.])
 @pytest.mark.parametrize("tone_hz", [800, 1000, 4000, 12000])
