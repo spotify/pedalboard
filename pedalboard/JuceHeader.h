3a54ee132a8570ea5f50223cd3b29cbf434c1b8d
Peter Sobot <psobot@spotify.com>
2025-05-12 18:47:48 -0400

    Avoid including definitions that we don't need.

--- pedalboard/JuceHeader.h
+++ pedalboard/JuceHeader.h
@@ -22,13 +22,39 @@
 
 #pragma once
 
+// NOTE(psobot): We include these modules conditionally to avoid unnecessary
+// dependencies on modules that are not available.
+
+#ifdef JUCE_MODULE_AVAILABLE_juce_audio_basics
 #include <juce_audio_basics/juce_audio_basics.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_audio_devices
 #include <juce_audio_devices/juce_audio_devices.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_audio_formats
 #include <juce_audio_formats/juce_audio_formats.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_audio_processors
 #include <juce_audio_processors/juce_audio_processors.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_core
 #include <juce_core/juce_core.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_data_structures
 #include <juce_data_structures/juce_data_structures.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_dsp
 #include <juce_dsp/juce_dsp.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_events
 #include <juce_events/juce_events.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_graphics
 #include <juce_graphics/juce_graphics.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_gui_basics
 #include <juce_gui_basics/juce_gui_basics.h>
+#endif
+#ifdef JUCE_MODULE_AVAILABLE_juce_gui_extra
+#include <juce_gui_extra/juce_gui_extra.h>
+#endif
