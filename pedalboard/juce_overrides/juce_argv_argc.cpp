0f7a681322853fb2850c2f16d6364689bf37601c
Peter Sobot <psobot@spotify.com>
2025-05-12 21:21:24 -0400

    Mock File::getSpecialLocation.

--- pedalboard/juce_overrides/juce_argv_argc.cpp
+++ pedalboard/juce_overrides/juce_argv_argc.cpp
@@ -2,5 +2,7 @@
 namespace juce {
 const char *const *juce_argv = nullptr;
 int juce_argc = 0;
+
+File File::getSpecialLocation(const SpecialLocationType type) { return {}; }
 } // namespace juce
 #endif
