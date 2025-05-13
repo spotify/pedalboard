8f873d1d544770bf2120f5591424a325bf520e0c
Peter Sobot <psobot@spotify.com>
2025-05-13 10:25:43 -0400

    D'oh.

--- pedalboard/juce_overrides/juce_wasm_Mocks.c
+++ pedalboard/juce_overrides/juce_wasm_Mocks.c
@@ -1,6 +1,5 @@
 #ifdef JUCE_WASM
-size_t wcsftime(wchar_t *__restrict __maxlen, size_t __maxlen,
-                const wchar_t *__restrict, const struct tm *__restrict) {
+size_t wcsftime(wchar_t *, size_t, const wchar_t *, const struct tm *) {
   return 0;
 }
 #endif
