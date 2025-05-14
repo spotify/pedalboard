0df2e81fd0548382aa7e2e2ffedef08d5ba3cd65
Peter Sobot <psobot@spotify.com>
2025-05-14 14:00:47 -0400

    Update docs to v0.9.17.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.9.16',
+    VERSION: 'v0.9.17',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
